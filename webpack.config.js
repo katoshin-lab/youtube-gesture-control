7b9f8373cbe05b6a199f3fd314c6f496a837bb5f
katoshin-lab <hatao0000000@outlook.com>
2022-03-10 09:27:24 +0900

    webpackの設定をメイン処理側とビュー側で分ける

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path')
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
-module.exports = {
-  mode: process.env.NODE_ENV ? "productions" : "development",
+const common = {
+  mode: process.env.NODE_ENV === 'development' ? "development" : "production",
   context: __dirname,
-  entry: {
-    background: path.join(__dirname, 'src', 'app.ts'),
-  },
   output: {
     path: `${__dirname}/dist`,
     filename: "[name].js"
@@ -23,10 +20,28 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-  devtool: 'inline-source-map',
+  watch: process.env.NODE_ENV === 'development',
+  cache: true,
+  devtool: process.env.NODE_ENV === 'development' ? 'inline-source-map' : undefined,
   plugins: [
     new CopyWebpackPlugin({
       patterns: [{ from: ".", to: ".", context: "public" }]
     })
   ],
 }
+
+const main = {
+  ...common,
+  entry: {
+    background: path.join(__dirname, 'src', 'app.ts'),
+  },
+};
+
+const view = {
+  ...common,
+  entry: {
+    view: path.join(__dirname, 'src', 'views', 'index.tsx')
+  }
+}
+
+module.exports = [main, view];
