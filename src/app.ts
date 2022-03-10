2f0c7704a97e838b2f6dcd67660877e39464b4b3
katoshin-lab <hatao0000000@outlook.com>
2022-03-10 09:28:37 +0900

    設定に併せてディレクトリやファイル名を変更

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 chrome.action.onClicked.addListener(async () => {
-  const url = chrome.runtime.getURL('test.html');
+  const url = chrome.runtime.getURL('index.html');
   await chrome.tabs.create({ url });
 });
---
+++ src/views/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Hello!!!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
