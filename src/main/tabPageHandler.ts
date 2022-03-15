8d79e8b64b17a52fe820caebbab625581da874f1
katoshin-lab <hatao0000000@outlook.com>
2022-03-15 11:33:54 +0900

    ビデオを停止するスクリプト

--- src/main/tabPageHandler.ts
+++ src/main/tabPageHandler.ts
@@ -1,34 +1,29 @@
-import type { videoElement } from '../types/tabPageHandler';
+// import type { videoElement } from '../types/tabPageHandler';
 
 export default class TabPageHander {
   protected static readonly youtubeVideoElementClass = '.video-stream .html5-main-video';
 
   public tab: chrome.tabs.Tab;
 
-  public videoElm: videoElement;
-
   constructor(tab: chrome.tabs.Tab) {
     this.tab = tab;
-    this.videoElm = this.getVideoElm();
   }
 
-  protected getVideoElm(): videoElement {
-    let videoElement: videoElement = null;
-    if (this.tab?.id) {
-      const getElm = () => document.querySelector('.html5-main-video');
-      this.executeScript<videoElement>(getElm, (res) => { console.log(res) })
-    } else {
-      this.noTabNotice();
-    }
-    console.log('videoElement = ', videoElement)
-    return videoElement;
+  public pauseVideo() {
+    this.executeScript(
+      () => {
+        const video = document.querySelector('.html5-main-video') as HTMLVideoElement
+        video?.pause();
+      },
+      (res) => console.log('pause', res)
+    );
   }
 
-  private executeScript<T>(func: () => T, callback: (res: chrome.scripting.InjectionResult[]) => void): void {
+  private executeScript<T = void>(func: () => T, callback: (res: chrome.scripting.InjectionResult[]) => void): void {
     if (typeof this.tab?.id === 'number') {
       chrome.scripting.executeScript(
         {
-          target: { tabId: this.tab.id, allFrames: true },
+          target: { tabId: this.tab.id },
           func
         },
         callback
