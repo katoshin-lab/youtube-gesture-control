c3ebafc908d5db1a05b1c50308ac97883a99dae7
katoshin-lab <hatao0000000@outlook.com>
2022-03-18 13:45:57 +0900

    カメラの表示と許可を追加

--- src/views/settings.tsx
+++ src/views/settings.tsx
@@ -4,15 +4,45 @@ import { FormControlLabel, Switch, } from '@material-ui/core';
 import styled from 'styled-components';
 import type Storage from '../types/storage';
 
-const StyledPopupContainer = styled.div`
+type StyledProps = { enableCamera: boolean };
+
+const StyledPopupContainer = styled.div<StyledProps>`
   padding: 1rem;
-  .switch, .video-container {
+  .switch, .io-wrapper {
     margin: 2rem 0;
   }
-  video {
-    width: 160px;
-    height: 90px;
-    transform: scale(-1, 1);
+  .io-wrapper {
+    display: flex;
+    margin-left: 1.5rem;
+    >div {
+      margin-left: 1.5rem;
+      width: 160px;
+      height: 90px;
+      border-radius: 4px;
+      border: 1px solid #cccccc;
+      video, canvas {
+        display: ${({ enableCamera }) => enableCamera ? 'block' : 'none'};
+        width: 100%;
+        height: 100%;
+        transform: scale(-1, 1);
+      }
+      span {
+        display: flex;
+        padding: .5rem;
+        height: 100%;
+        align-items: center;
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+    >div:first-of-type {
+      margin-left: 0;
+    }
+  }
+  @media (prefers-color-scheme: dark) {
+    .io-wrapper >div {
+      border-color: #333333;
+    }
   }
 `;
 
@@ -20,13 +50,16 @@ const Settings = () => {
   const { useState, useEffect, useRef } = React;
 
   const videoElm = useRef<HTMLVideoElement>(null);
+  const canvasElm = useRef<HTMLCanvasElement>(null);
 
   const [enabled, setEnabled] = useState<boolean>(false);
+  const [enableCamera, setEnableCamera] = useState<boolean>(false)
 
   const setVideoStream = async () => {
     if (navigator.mediaDevices) {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
       videoElm.current!.srcObject = stream;
+      stream && setEnableCamera(true)
     }
   }
 
@@ -44,7 +77,7 @@ const Settings = () => {
   };
 
   return (
-    <StyledPopupContainer>
+    <StyledPopupContainer enableCamera={enableCamera}>
       <h1>Welcome to Youtube Gesture Contol 👋</h1>
       <FormControlLabel
         label={enabled ? 'ENABLE (Your gesture works when the active tab url is \'www.youtube.com\'.)' : 'DISABLE'}
@@ -58,13 +91,19 @@ const Settings = () => {
         }
         className='switch'
       />
-      <div className='video-container'>
-        <video
-          ref={videoElm}
-          autoPlay
-          playsInline
-          muted
-        />
+      <div className="io-wrapper">
+        <div>
+          <video
+            ref={videoElm}
+            autoPlay
+            playsInline
+            muted
+          />
+          <span onClick={setVideoStream}>{ enableCamera || 'Please enable camera access.' }</span>
+        </div>
+        <div>
+          <canvas ref={canvasElm} />
+        </div>
       </div>
     </StyledPopupContainer>
   );
