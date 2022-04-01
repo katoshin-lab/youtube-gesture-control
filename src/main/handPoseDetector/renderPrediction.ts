30d4624f680a5af431bc537153e7857e5bc19bcd
katoshin-lab <hatao0000000@outlook.com>
2022-04-01 17:54:34 +0900

    全てのキーポイントを表示

--- src/main/handPoseDetector/renderPrediction.ts
+++ src/main/handPoseDetector/renderPrediction.ts
@@ -1,6 +1,22 @@
 import config from '../../config';
 import { Keypoint } from '@tensorflow-models/hand-pose-detection';
 
+// const fingerLookupIndices = {
+//   thumb: [0, 1, 2, 3, 4],
+//   indexFinger: [0, 5, 6, 7, 8],
+//   middleFinger: [0, 9, 10, 11, 12],
+//   ringFinger: [0, 13, 14, 15, 16],
+//   pinky: [0, 17, 18, 19, 20],
+// };
+
+const connections = [
+  [0, 1], [1, 2], [2, 3], [3,4],
+  [0, 5], [5, 6], [6, 7], [7, 8],
+  [0, 9], [9, 10], [10, 11], [11, 12],
+  [0, 13], [13,14], [14, 15], [15, 16],
+  [0, 17], [17, 18],[18, 19], [19,20]
+];
+
 export default class RenderPrediction {
   public static fpsCounter = 0;
 
@@ -30,21 +46,24 @@ export default class RenderPrediction {
   }
 
   protected prerenderStroke(keypoints: Keypoint[]): void {
-    const start = keypoints.find(({ name }) => name === 'index_finger_mcp');
-    const end = keypoints.find(({ name }) => name === 'index_finger_tip');
-    if (start && end && this.preCtx) {
+    if (this.preCtx) {
       this.preCtx.beginPath();
-      this.preCtx.moveTo(
-        Math.round((config.capture.width * 5 - start.x) / 2.5),
-        Math.round(start.y / 2.5)
-      );
-      this.preCtx.lineTo(
-        Math.round((config.capture.width * 5 - end.x) / 2.5),
-        Math.round(end.y / 2.5)
-      );
-      console.log('from: ', Math.round((config.capture.width * 5 - start.x) / 2.5), Math.round(start.y / 2.5), 'to: ', Math.round((config.capture.width * 5 - end.x) / 2.5), Math.round(end.y / 2.5))
-      this.preCtx.strokeStyle = "green";
-      this.preCtx.lineWidth = 3;
+      for (let i = 0; i < connections.length; i++) {
+        const [startIndex, endIndex] = connections[i];
+        const start = keypoints[startIndex];
+        const end = keypoints[endIndex];
+        this.preCtx.moveTo(
+          Math.round((config.capture.width * 5 - start.x) / 2.5),
+          Math.round(start.y / 2.5)
+        );
+        this.preCtx.lineTo(
+          Math.round((config.capture.width * 5 - end.x) / 2.5),
+          Math.round(end.y / 2.5)
+        );
+        // console.log('from: ', Math.round((config.capture.width * 5 - start.x) / 2.5), Math.round(start.y / 2.5), 'to: ', Math.round((config.capture.width * 5 - end.x) / 2.5), Math.round(end.y / 2.5))
+        this.preCtx.strokeStyle = "green";
+        this.preCtx.lineWidth = 3;
+      }
       this.preCtx.stroke();
     }
   }
